283a627a6d0696faa5d90996d28be834de656bac
Joonas Hyvärinen <joonas.hyvarinen@ictpro.labra>
2020-11-18 12:17:32 +0200

    Trace

--- Harjoitus_02.ino
+++ Harjoitus_02.ino
@@ -3,11 +3,17 @@
 #define LEDOFF HIGH
 
 void setup(){
-  pinMode(LED, OUTPUT);  
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);  
 }
 
-int viive = 500;
+int viive = 1000;
 
 void loop(){
   digitalWrite(LED, LEDON);
+  Serial.println("Palaa");
+  delay(viive);
+  digitalWrite(LED, LEDOFF);
+  Serial.println("Sammuu");
+  delay(viive);
 }
